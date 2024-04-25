19ceea38e94b45ee440cf4d6e4f16c9c855f8772
Ruben Home PC <rubenn.mmartinezz@gmail.com>
2024-04-25 23:43:42 +0200

    Add Email and Password VO and install new dependencies

---
+++ src/VO/Email.ts
@@ -0,0 +1,20 @@
+export class Email {
+
+    private readonly validEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    constructor(
+        private _value: string
+    ) {
+        this.ensureIsValid(_value);
+    }
+
+    get value(): string {
+        return this._value;
+    }
+
+    private ensureIsValid(value: string): void {
+        if (!this.validEmailRegex.test(value)) {
+            throw new Error('Invalid email');
+        }
+    }
+}
---
+++ src/VO/Password.ts
@@ -0,0 +1,52 @@
+import bcrypt from 'bcryptjs';
+
+export class Password {
+
+    private _hash: string;
+    private _raw: string | null;
+
+    private constructor(
+        raw: string | null,
+        hash: string,
+    ) {
+        this._raw = raw;
+        this._hash = hash;
+    }
+
+    public static fromRaw(raw: string): Password {
+        this.ensureIsValid(raw)
+        const hash = bcrypt.hashSync(raw, 10);
+
+        return new Password(raw, hash);
+    }
+
+    public static fromHash(hash: string): Password {
+        return new Password(null, hash);
+    }
+
+    public get hash(): string {
+        return this._hash;
+    }
+
+    public hasRawPassword(): boolean {
+        return this._raw !== null;
+    }
+
+    public isEqual(password: Password): boolean {
+        if (!password.hasRawPassword() && !this.hasRawPassword()) {
+            throw new Error("Passwords doesn't match");
+        }
+
+        if (this.hasRawPassword()) {
+            return bcrypt.compareSync(this._raw!, password.hash);
+        }
+
+        return password.isEqual(this);
+    }
+
+    private static ensureIsValid(value: string): void {
+        if(value && value.length === 0) {
+            throw new Error('Password needed')
+        }
+    }
+}
