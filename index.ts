96172c2d8f27287173bee9f438cf38ce17b99c96
Ruben Home PC <rubenn.mmartinezz@gmail.com>
2024-05-07 01:24:18 +0200

    FIX ANOTHER

--- index.ts
+++ index.ts
@@ -13,8 +13,7 @@ mongo.init();
 
 // Middlewares
 app.use(express.json());
-// app.use(cors({ origin: "https://task-track-frontend.vercel.app" }));
-app.use(cors());
+app.use(cors({ origin: "https://task-track-frontend.vercel.app" }));
 app.use((req, _, next) => {
   console.log(`${req.method} ${req.path}`);
   next();
