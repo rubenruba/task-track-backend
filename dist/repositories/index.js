7f0021e00202e58a4826ec4ce68df814e5d7f19c
Ruben Home PC <rubenn.mmartinezz@gmail.com>
2024-05-06 23:32:27 +0200

    Delete dist folder

--- dist/repositories/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.taskMongoRepository = exports.userMongoRepository = void 0;
-const taskMongo_repository_1 = require("./task/taskMongo.repository");
-const userMongo_repository_1 = require("./user/userMongo.repository");
-exports.userMongoRepository = new userMongo_repository_1.UserMongoRepository();
-exports.taskMongoRepository = new taskMongo_repository_1.TaskMongoRepository();
