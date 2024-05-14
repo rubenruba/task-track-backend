4c9a15a7a6af695ef01eb17a5732b62553530102
Ruben Home PC <rubenn.mmartinezz@gmail.com>
2024-05-15 00:26:51 +0200

    Add listRouter to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { userRouter } from "./user/user.router";
 import { taskRouter } from "./task/task.router";
+import { listRouter } from "./list/list.router";
 
 const indexRouter = Router();
 
 indexRouter.use('/user', userRouter);
 indexRouter.use('/task', taskRouter);
+indexRouter.use('/list', listRouter);
 
 export default indexRouter;
